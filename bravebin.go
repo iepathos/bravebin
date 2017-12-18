859275865fe906206f161ba92f5d1b1bf24e7664
Glen Baker <iepathos@gmail.com>
2017-12-17 17:41:03 -0800

    more organization

--- bravebin.go
+++ bravebin.go
@@ -3,87 +3,12 @@ package main
 import (
 	"fmt"
 	core "github.com/iepathos/bravebin/core"
-	"io/ioutil"
-	"log"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 )
-
-func removeDuplicates(elements []string) []string {
-	// Use map to record duplicates as we find them.
-	encountered := map[string]bool{}
-	result := []string{}
-
-	for v := range elements {
-		if encountered[elements[v]] == true {
-			// Do not add duplicate.
-		} else {
-			// Record this element as an encountered element.
-			encountered[elements[v]] = true
-			// Append to result slice.
-			result = append(result, elements[v])
-		}
-	}
-	// Return the new slice.
-	return result
-}
-
-func GenerateGoMainPackage(imports []string, instructions []string) string {
-	// package main
-	// import (
-	// {{ imports }}
-	// )
-	// func main() {
-	// {{program}}
-	// }
-	imports = removeDuplicates(imports)
-	content := []string{
-		"package main",
-		"import (",
-		strings.Join(imports, ",\n"),
-		")",
-		"func main() {",
-		strings.Join(instructions, "\n"),
-		"}",
-	}
-	tmpfile, err := ioutil.TempFile("/tmp", "bravebin")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if _, err := tmpfile.Write([]byte(strings.Join(content, "\n"))); err != nil {
-		log.Fatal(err)
-	}
-	if err := tmpfile.Close(); err != nil {
-		log.Fatal(err)
-	}
-	newname := tmpfile.Name() + ".go"
-	os.Rename(tmpfile.Name(), newname)
-	return newname
-}
-
-func BuildGofile(gofile string) string {
-	// go build {{ gofile }}
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	os.Chdir(filepath.Dir(gofile))
-	cmd := exec.Command("sh", "-c", fmt.Sprintf("go build %s", gofile))
-	_, err = cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	os.Chdir(cwd)
-	return gofile[0 : len(gofile)-3]
-}
 
 func main() {
 	imports, instructions := core.CreateEchoInstruction("make devops better and braver")
 	imports2, instructions2 := core.CreateEchoInstruction("and another instruction!")
-	gofile := GenerateGoMainPackage(append(imports, imports2...), append(instructions, instructions2...))
-	gobin := BuildGofile(gofile)
+	gofile := core.GenerateGoMainPackage(append(imports, imports2...), append(instructions, instructions2...))
+	gobin := core.BuildGofile(gofile)
 	fmt.Println(gobin)
 }
---
+++ core/build.go
@@ -0,0 +1,80 @@
+package bravebin
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func removeDuplicates(elements []string) []string {
+	// Use map to record duplicates as we find them.
+	encountered := map[string]bool{}
+	result := []string{}
+
+	for v := range elements {
+		if encountered[elements[v]] == true {
+			// Do not add duplicate.
+		} else {
+			// Record this element as an encountered element.
+			encountered[elements[v]] = true
+			// Append to result slice.
+			result = append(result, elements[v])
+		}
+	}
+	// Return the new slice.
+	return result
+}
+
+func GenerateGoMainPackage(imports []string, instructions []string) string {
+	// package main
+	// import (
+	// {{ imports }}
+	// )
+	// func main() {
+	// {{program}}
+	// }
+	imports = removeDuplicates(imports)
+	content := []string{
+		"package main",
+		"import (",
+		strings.Join(imports, ",\n"),
+		")",
+		"func main() {",
+		strings.Join(instructions, "\n"),
+		"}",
+	}
+	tmpfile, err := ioutil.TempFile("/tmp", "bravebin")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := tmpfile.Write([]byte(strings.Join(content, "\n"))); err != nil {
+		log.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		log.Fatal(err)
+	}
+	newname := tmpfile.Name() + ".go"
+	os.Rename(tmpfile.Name(), newname)
+	return newname
+}
+
+func BuildGofile(gofile string) string {
+	// go build {{ gofile }}
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Chdir(filepath.Dir(gofile))
+	cmd := exec.Command("sh", "-c", fmt.Sprintf("go build %s", gofile))
+	_, err = cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Chdir(cwd)
+	return gofile[0 : len(gofile)-3]
+}
