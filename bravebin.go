bc75d341f5592b79e4fe33aaa9684dcd49b7da7a
Glen Baker <iepathos@gmail.com>
2017-12-19 20:23:06 -0800

    cleanup dead code

--- bravebin.go
+++ bravebin.go
@@ -7,18 +7,13 @@ import (
 )
 
 func main() {
-	// imports, instructions := core.DebugMsgInstruction("make devops better and braver")
-	// imports2, instructions2 := core.DebugMsgInstruction("and another instruction!")
-	// gofile := core.GenerateGoMainPackage(append(imports, imports2...), append(instructions, instructions2...))
-	// gobin := core.BuildGofile(gofile)
-	// fmt.Println(gobin)
 	if len(os.Args) < 2 {
 		fmt.Println("Missing playbook, pass yaml file as first argument to bravebin")
 		os.Exit(2)
 	}
 	filename := string(os.Args[1])
 	parser := core.BraveParser{
-		filename, //"/home/cha0s/go/src/github.com/iepathos/bravebin/samples/simple_debug.yml",
+		filename,
 		[]core.Instruction{},
 	}
 	instructions := parser.Parse()
